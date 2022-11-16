ef767ae75d542739edae4f51397d5fd6a0503275
Elanis <elanis@hotmail.com>
2022-11-16 19:00:52 +0100

    Fix cleaning task

--- Dysnomia.DownStatus.Persistance/Implementations/MonitoringEntryHistoryEntriesRepository.cs
+++ Dysnomia.DownStatus.Persistance/Implementations/MonitoringEntryHistoryEntriesRepository.cs
@@ -20,35 +20,33 @@ namespace Dysnomia.DownStatus.Persistance.Implementations {
 		}
 
 		public async Task CleanUselessEntries() {
-			await Task.Run(() => {
-				context.MonitoringEntry.FromSqlRaw(@"
-					DELETE
-					FROM ""MonitoringHistory"" as current_history
-					USING ""MonitoringHistory"" as previous_history, ""MonitoringHistory"" as next_history
-					WHERE 
-						previous_history.""MonitoringEntryAppId"" = current_history.""MonitoringEntryAppId"" AND 
-						previous_history.""MonitoringEntryName"" = current_history.""MonitoringEntryName"" AND
-						previous_history.""Status"" = current_history.""Status"" AND
-						previous_history.""Message"" = current_history.""Message"" AND
-						previous_history.""Date"" = (
-							SELECT MAX(tableA.""Date"") FROM ""MonitoringHistory"" as tableA 
-							WHERE tableA.""Date"" < current_history.""Date""
-							AND previous_history.""MonitoringEntryAppId"" = tableA.""MonitoringEntryAppId""
-							AND previous_history.""MonitoringEntryName"" = tableA.""MonitoringEntryName""
-						) AND
-						next_history.""MonitoringEntryAppId"" = current_history.""MonitoringEntryAppId"" AND 
-						next_history.""MonitoringEntryName"" = current_history.""MonitoringEntryName"" AND
-						next_history.""Status"" = current_history.""Status"" AND
-						next_history.""Message"" = current_history.""Message"" AND
-						next_history.""Date"" = (
-							SELECT MIN(tableB.""Date"")
-							FROM ""MonitoringHistory"" as tableB
-							WHERE tableB.""Date"" > current_history.""Date""
-							AND next_history.""MonitoringEntryAppId"" = tableB.""MonitoringEntryAppId""
-							AND next_history.""MonitoringEntryName"" = tableB.""MonitoringEntryName""
-						)
-				");
-			});
+			await context.Database.ExecuteSqlRawAsync(@"
+				DELETE
+				FROM ""MonitoringHistory"" as current_history
+				USING ""MonitoringHistory"" as previous_history, ""MonitoringHistory"" as next_history
+				WHERE 
+					previous_history.""MonitoringEntryAppId"" = current_history.""MonitoringEntryAppId"" AND 
+					previous_history.""MonitoringEntryName"" = current_history.""MonitoringEntryName"" AND
+					previous_history.""Status"" = current_history.""Status"" AND
+					previous_history.""Message"" = current_history.""Message"" AND
+					previous_history.""Date"" = (
+						SELECT MAX(tableA.""Date"") FROM ""MonitoringHistory"" as tableA 
+						WHERE tableA.""Date"" < current_history.""Date""
+						AND previous_history.""MonitoringEntryAppId"" = tableA.""MonitoringEntryAppId""
+						AND previous_history.""MonitoringEntryName"" = tableA.""MonitoringEntryName""
+					) AND
+					next_history.""MonitoringEntryAppId"" = current_history.""MonitoringEntryAppId"" AND 
+					next_history.""MonitoringEntryName"" = current_history.""MonitoringEntryName"" AND
+					next_history.""Status"" = current_history.""Status"" AND
+					next_history.""Message"" = current_history.""Message"" AND
+					next_history.""Date"" = (
+						SELECT MIN(tableB.""Date"")
+						FROM ""MonitoringHistory"" as tableB
+						WHERE tableB.""Date"" > current_history.""Date""
+						AND next_history.""MonitoringEntryAppId"" = tableB.""MonitoringEntryAppId""
+						AND next_history.""MonitoringEntryName"" = tableB.""MonitoringEntryName""
+					)
+			");
 		}
 
 		public IEnumerable<MonitoringEntryHistoryEntry?> GetEntriesForHomepage(int amount) {
