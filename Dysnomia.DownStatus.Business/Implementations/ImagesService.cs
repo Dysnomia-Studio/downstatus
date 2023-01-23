68b4a20b614fedef9f412b3fba21597023592b67
Elanis <elanis@hotmail.com>
2023-01-23 21:11:02 +0100

    Serverside cache

--- Dysnomia.DownStatus.Business/Implementations/ImagesService.cs
+++ Dysnomia.DownStatus.Business/Implementations/ImagesService.cs
@@ -1,60 +1,66 @@
 ﻿using Dysnomia.DownStatus.Business.Interfaces;
 using Dysnomia.DownStatus.Persistance.Interfaces;
 
+using Microsoft.Extensions.Caching.Memory;
+
 namespace Dysnomia.DownStatus.Business.Implementations {
-	public class ImagesService : IImagesService {
-		private readonly IAppsRepository _appsRepository;
-		private readonly HttpClient _httpClient;
+    public class ImagesService : IImagesService {
+        private readonly IAppsRepository _appsRepository;
+        private readonly HttpClient _httpClient;
+        private readonly IMemoryCache _memoryCache;
 
-		public ImagesService(IAppsRepository appsRepository, HttpClient httpClient) {
-			_appsRepository = appsRepository;
-			_httpClient = httpClient;
-		}
+        public ImagesService(IAppsRepository appsRepository, HttpClient httpClient, IMemoryCache memoryCache) {
+            _appsRepository = appsRepository;
+            _httpClient = httpClient;
+            _memoryCache = memoryCache;
+        }
 
-		private string GetContentTypeFromExtension(string extension) {
-			var contentType = "";
-			switch (extension.ToLower().Substring(1)) {
-				case "bmp":
-					contentType = "image/bmp";
-					break;
-				case "jpg":
-				case "jpeg":
-					contentType = "image/jpeg";
-					break;
-				case "gif":
-					contentType = "image/gif";
-					break;
-				case "ico":
-					contentType = "image/vnd.microsoft.icon";
-					break;
-				case "png":
-					contentType = "image/png";
-					break;
-				case "svg":
-					contentType = "image/svg+xml";
-					break;
-				case "webp":
-					contentType = "image/webp";
-					break;
-			}
+        private string GetContentTypeFromExtension(string extension) {
+            var contentType = "";
+            switch (extension.ToLower().Substring(1)) {
+                case "bmp":
+                    contentType = "image/bmp";
+                    break;
+                case "jpg":
+                case "jpeg":
+                    contentType = "image/jpeg";
+                    break;
+                case "gif":
+                    contentType = "image/gif";
+                    break;
+                case "ico":
+                    contentType = "image/vnd.microsoft.icon";
+                    break;
+                case "png":
+                    contentType = "image/png";
+                    break;
+                case "svg":
+                    contentType = "image/svg+xml";
+                    break;
+                case "webp":
+                    contentType = "image/webp";
+                    break;
+            }
 
-			return contentType;
-		}
+            return contentType;
+        }
 
-		public async Task<(byte[], string, string)> GetImageFromServiceKey(string key) {
-			var src = await _appsRepository.GetImageSrc(key);
-			if (src == null) {
-				throw new ArgumentNullException(key);
-			}
+        public async Task<(byte[], string, string)> GetImageFromServiceKey(string key) {
+            return await _memoryCache.GetOrCreateAsync<(byte[], string, string)>("img-" + key, async (x) => {
+                var src = await _appsRepository.GetImageSrc(key);
+                if (src == null) {
+                    throw new ArgumentNullException(key);
+                }
 
-			HttpResponseMessage response = await _httpClient.GetAsync(src);
-			byte[] content = await response.Content.ReadAsByteArrayAsync();
+                HttpResponseMessage response = await _httpClient.GetAsync(src);
+                byte[] content = await response.Content.ReadAsByteArrayAsync();
 
-			var extension = Path.GetExtension(src);
-			var contentType = GetContentTypeFromExtension(extension);
-			var fileName = key + extension;
+                var extension = Path.GetExtension(src);
+                var contentType = GetContentTypeFromExtension(extension);
+                var fileName = key + extension;
 
-			return (content, contentType, fileName);
-		}
-	}
+                return (content, contentType, fileName);
+            });
+        }
+    }
 }
--- Dysnomia.DownStatus.WebApp/Controllers/ImageController.cs
+++ Dysnomia.DownStatus.WebApp/Controllers/ImageController.cs
@@ -3,25 +3,24 @@
 using Microsoft.AspNetCore.Mvc;
 
 namespace Dysnomia.DownStatus.WebApp.Controllers {
-	[ApiController]
-	[Route("[controller]")]
-	public class ImageController : ControllerBase {
-		private readonly IImagesService _imagesService;
-		public ImageController(IImagesService imagesService) {
-			_imagesService = imagesService;
-		}
+    [ApiController]
+    [Route("[controller]")]
+    public class ImageController : ControllerBase {
+        private readonly IImagesService _imagesService;
+        public ImageController(IImagesService imagesService) {
+            _imagesService = imagesService;
+        }
 
-		[HttpGet("{serviceKey}")]
-		[ResponseCache(Duration = 604800)] // Cache this picture for a week clientside
-										   // TODO: serverside cache
-		public async Task<IActionResult> GetImage(string serviceKey) {
-			try {
-				(byte[] content, string contentType, string name) = await _imagesService.GetImageFromServiceKey(serviceKey);
+        [HttpGet("{serviceKey}")]
+        [ResponseCache(Duration = 604800)] // Cache this picture for a week clientside
+        public async Task<IActionResult> GetImage(string serviceKey) {
+            try {
+                (byte[] content, string contentType, string name) = await _imagesService.GetImageFromServiceKey(serviceKey);
 
-				return File(content, contentType);
-			} catch {
-				return NotFound();
-			}
-		}
-	}
+                return File(content, contentType);
+            } catch {
+                return NotFound();
+            }
+        }
+    }
 }
