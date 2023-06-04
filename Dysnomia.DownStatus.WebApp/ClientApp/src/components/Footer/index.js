476e3448ff02bf905823a31f4fe0f923f0a649f4
Elanis <elanis@hotmail.com>
2023-06-04 09:17:48 +0200

    Add link to privacy policy

--- Dysnomia.DownStatus.WebApp/ClientApp/src/components/Footer/index.js
+++ Dysnomia.DownStatus.WebApp/ClientApp/src/components/Footer/index.js
@@ -1,8 +1,9 @@
 import './index.css';
 
 export default function Footer() {
-	return (<footer>
-		<p>Created by <a href="https://dysnomia.studio/">Dysnomia</a> - Copyright 2022</p>
-		<p>Our other websites: <a href="https://achieve.games">Achieve.games</a> - <a href="https://dehash.me">Dehash.me</a></p>
-	</footer>);
+    return (<footer>
+        <p>Created by <a href="https://dysnomia.studio/">Dysnomia</a> - Copyright 2022</p>
+        <p>Our other websites: <a href="https://achieve.games">Achieve.games</a> - <a href="https://dehash.me">Dehash.me</a></p>
+        <p><a href="https://dysnomia.studio/privacy/#downstat-us">Privacy policy</a></p>
+    </footer>);
 }
